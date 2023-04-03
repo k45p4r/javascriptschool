e48e565d7718b697d664d653dc79139e28eda9a1
k45p4r <kaspar.vainonen@metropolia.fi>
2023-04-03 16:14:44 +0300

    Up

--- jsmod01/js/1.3.js
+++ jsmod01/js/1.3.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const n1 = prompt("Anna numero:")
-const n2 = prompt("Anna toinen numero:")
-const n3 = prompt("Anna kolmas numero:")
+const n1str = prompt("Anna numero:")
+const n2str = prompt("Anna toinen numero:")
+const n3str = prompt("Anna kolmas numero:")
+const n1 = parseInt(n1str)
+const n2 = parseInt(n2str)
+const n3 = parseInt(n3str)
+
+document.querySelector('#sum').innerHTML =  n1 + n2 + n3;
+document.querySelector('#product').innerHTML =  n1 * n2 * n3;
+document.querySelector('#avg').innerHTML = (n1 + n2 + n3) / 3;
