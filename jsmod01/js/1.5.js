3d9d3853d13ab1414fea3f3fa3df86d71b1d74eb
k45p4r <kaspar.vainonen@metropolia.fi>
2023-04-04 19:24:09 +0300

    Up

--- jsmod01/js/1.5.js
+++ jsmod01/js/1.5.js
@@ -1,16 +1,11 @@
-'use strict';
-let yearfax = 0
+
+
 const yearstr = prompt("Anna vuosi:")
 const year = parseInt(yearstr)
 
-if (year % 400 === 0 && year % 100 === 0)
-  yearfax += 1;
-else if (year % 4 === 0)
-  yearfax += 1;
-
-
-if (yearfax < 0)
-  document.querySelector('#year').innerHTML = year + ' on karkausvuosi.';
-else
-  document.querySelector('#year').innerHTML = year + ' ei ole karkausvuosi.';
-
+if (year % 100 === 0 && year % 400 === 0 || year % 4 === 0) {
+  document.querySelector('#year').innerHTML = yearstr + ' on karkausvuosi.';
+}
+else {
+  document.querySelector('#year').innerHTML = yearstr + ' ei ole karkausvuosi.';
+}
