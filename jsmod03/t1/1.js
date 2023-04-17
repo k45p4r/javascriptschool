a6917c2fe22d893b27dd06bc11a217362b9ed11e
k45p4r <kasparv@metropolia.fi>
2023-04-17 17:11:23 +0300

    up

---
+++ jsmod03/t1/1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let target = document.querySelector('#target')
+const html = `
+<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>
+`;
+target.innerHTML = html;
+target.classList.add('my-list');
